2c36870a8d05f911181618c6731665fdccc71077
Rakshika Raju <rakshikaraju@gmail.com>
2023-08-06 13:01:03 -0700

    Add files via upload

---
+++ Backend/users/users-controller.js
@@ -0,0 +1,123 @@
+// import everyone from './users.js'
+import * as dao from './users-dao.js'
+import {findUserByCreds, findUserByUsername} from './users-dao.js'
+import jwt from 'jsonwebtoken';
+import protect from './middleware/auth-middleware.js'
+
+
+let currentUser = null
+
+// let users = everyone
+const findAllUsers = async (req,res) => {
+    const users = await dao.findAllUsers()
+    res.json(users)
+}
+
+const findUserById = async (req, res) => {
+    const userId = req.params.uid;
+    const user = await dao.findUserById(userId)
+    //if (user != res.user) throw error
+    if(user) {
+        res.json(user);
+        return
+    }
+    res.sendStatus(404)
+}
+
+const createUser = async (req, res) => {
+    const newUser = req.body;
+    const user = await dao.createUser(newUser)
+    res.json(user);
+}
+
+const deleteUser = async (req, res) => {
+    const userId = req.params.uid;
+    const status = await dao.deleteUser(req.user.id) //req.user.id
+    res.json(status);
+}
+  
+const updateUser = async (req, res) => {
+    const uid = req.params.uid
+    const updates = req.body
+    const status = await dao.updateUser(uid,  updates)
+    res.json(status)
+}
+
+const register = async (req,res) => {
+    const user = req.body
+    const existingUser = await findUserByUsername(user.userName)
+   
+    if (existingUser) {
+        res.sendStatus(403)
+        return
+    }
+
+
+    const userToCreate = await dao.createUser(user)
+    // req.session['currentUser'] = userToCreate
+
+    if (userToCreate.role != 'DONOR') {
+        currentUser = userToCreate
+    }
+    console.log("current user in controller: ", currentUser)
+    res.json({
+        userName: userToCreate.userName,
+        email: userToCreate.email,
+        token: generateToken(userToCreate.userName),
+        role: userToCreate.role,
+        status: userToCreate.status
+    })
+}
+
+const login = async (req,res) => {
+    const credentials = req.body
+    const existingUser = await findUserByCreds(credentials.userName, credentials.password)
+    console.log('existingUser: ', existingUser);
+    if (!existingUser) {
+        res.status(403).send('User does not exist')
+        return
+    }
+    // req.session['currentUser'] = existingUser
+    currentUser = existingUser
+    res.json({ _id: existingUser._id,
+        userName: existingUser.userName,
+        email: existingUser.email,
+        token: generateToken(existingUser.userName),
+        role: existingUser.role,
+        status: existingUser.status})
+}
+
+// const profile = (req,res) => {
+//     if (req.session['currentUser']) {
+//         // res.send(req.session(['currentUser']))
+//         res.send(currentUser)
+//     }
+//     else {
+//         res.sendStatus(403)
+//     }
+// }
+
+const logout = (req,res) => {
+    // req.session.destroy()
+    req.user = null
+    res.sendStatus(200)
+}
+
+const usersController = (app) => {
+
+    app.post('/users', createUser)
+    app.get('/users', findAllUsers)
+    app.delete('/users/:uid', deleteUser)
+    app.get('/users/:uid', findUserById);
+    app.put('/users/:uid', updateUser)
+    app.post('/register', register)
+    app.post('/login', login)
+    // app.post('/profile', profile)
+    app.post('/logout', logout)
+}
+
+const generateToken = (userName) => {
+    return jwt.sign({userName}, "abcd1234", {expiresIn: '5m'})
+}
+
+export default usersController;
---
+++ Backend/users/users-dao.js
@@ -0,0 +1,49 @@
+import usersModel from "./schema/users-model.js";
+import donorsModel from "./schema/donors-model.js";
+import customersModel from "./schema/customers-model.js";
+
+export const createUser = (user) => {
+    if (user.role == 'DONOR') {
+        user.profilePhoto = 'profilePhoto.png',
+        user.coverPhoto = 'coverPhoto.jpeg'
+        user.description = "Money is not the only commodity that is fun to give. We can give time, we can give our expertise, we can give our love, or simply give a smile. What does that cost? The point is, none of us can ever run out of something worthwhile to give.",
+        user.storeTimings = "9am - 5pm",
+        user.rating = 0.0,
+        user.inventory = {"pizza": 0}
+        user.likes = 0
+        user.liked = false
+        donorsModel.create(user)
+    } else if (user.role == 'CUSTOMER') {
+        user.profilePhoto = 'profilePhoto.png'
+        user.about = "There is no exercise better for the heart than reaching down and lifting people up."
+        customersModel.create(user)
+    }
+    return usersModel.create(user)
+}
+
+export const findAllUsers = () => {
+    return usersModel.find()
+}
+
+export const findUserById = (uid) => {
+    return usersModel.findById(uid)
+}
+
+export const findUserByUsername = (userName) => {
+    const user = usersModel.findOne({userName})
+    return user
+}
+
+export const findUserByCreds = (userName, password) => {
+    const user = usersModel.findOne({userName, password},
+        {password: false})
+        return user
+}
+
+export const deleteUser = (uid) => {
+    return usersModel.deleteOne({_id: uid})
+}
+
+export const updateUser = (uid, userUpdates) =>
+    usersModel.updateOne({_id: uid},
+        {$set: userUpdates})
